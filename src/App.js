ea8f18822340ce9b9a9ab7c44e74781080d7d496
blixon <xawier700@gmail.com>
2022-01-10 19:53:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return <>
+  <header className='flex fl-col'>
+    <h1>Github profile browser</h1>
+    <div id="search"></div>
+  </header>
+  <main>
+    <aside></aside>
+    <section className='flex fl-wrap' >
+      {/* <div className="repo-card"></div>
+      <div className="repo-card"></div>
+      <div className="repo-card"></div>
+      <div className="repo-card"></div> */}
+    </section>
+  </main>
+  </>
+}
+
+export default App;
